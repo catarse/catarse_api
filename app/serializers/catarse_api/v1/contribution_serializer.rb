e1fa2cb285c3b8f0fafbc4639d9f075396590381
Antônio Roberto <forevertonny@gmail.com>
2014-12-10 10:46:23 -0200

    adjust contribution serializer

--- app/serializers/catarse_api/v1/contribution_serializer.rb
+++ app/serializers/catarse_api/v1/contribution_serializer.rb
@@ -6,8 +6,35 @@ module CatarseApi
         :name,
         :anonymous,
         :value,
-        :reward_id
+        :reward_id,
+        :email,
+        :address_street,
+        :address_number,
+        :address_neighbourhood,
+        :address_zip_code,
+        :address_city,
+        :address_state,
+        :address_complement,
+        :phone_number,
+        :user_id,
+        :confirmed_at,
+        :platform_fee,
+        :payer_email,
+        :payment_method,
+        :cpf
 
+
+      def platform_fee
+        return object.value * CatarseSettings[:catarse_fee].to_f if current_user_is_owner?
+      end
+
+      def payment_method
+        return object.payment_method if current_user_is_owner?
+      end
+
+      def payer_email
+        return object.payer_email if current_user_is_owner?
+      end
 
       def name
         return user.display_name if current_user_is_owner?
@@ -15,6 +42,54 @@ module CatarseApi
         unless object.anonymous?
           user.display_name
         end
+      end
+
+      def user_id
+        return object.user_id if current_user_is_owner?
+
+        unless object.anonymous?
+          object.user_id
+        end
+      end
+
+      def address_state
+        object.address_state || user.address_state if current_user_is_owner?
+      end
+
+      def address_complement
+        object.address_complement || user.address_complement if current_user_is_owner?
+      end
+
+      def address_city
+        object.address_city || user.address_city if current_user_is_owner?
+      end
+
+      def cpf
+        object.payer_document || user.cpf if current_user_is_owner?
+      end
+
+      def phone_number
+        object.address_phone_number || user.phone_number if current_user_is_owner?
+      end
+
+      def address_zip_code
+        object.address_zip_code || user.address_zip_code if current_user_is_owner?
+      end
+
+      def address_number
+        object.address_number || user.address_number if current_user_is_owner?
+      end
+
+      def address_neighbourhood
+        object.address_neighbourhood || user.address_neighbourhood if current_user_is_owner?
+      end
+
+      def address_street
+        object.address_street || user.address_street if current_user_is_owner?
+      end
+
+      def email
+        user.email if current_user_is_owner?
       end
 
       def value
